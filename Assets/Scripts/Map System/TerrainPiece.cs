0bcda30a224fc840e65611b467348e1e2bf45acb
QuinnGriffin <quinng15@gmail.com>
2024-05-13 21:21:37 +0100

    Terrain Tiling System. Includes a static event for collision data.

---
+++ Assets/Scripts/Map System/TerrainPiece.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TerrainPiece
+{
+    public SpriteRenderer objectRenderer; //Used as the handle for the actual gameobject.
+    public bool isInUse; //Is the piece on the map? If not, it is back in the pool.
+    public float Radius { get; private set; }
+
+    public void PlacePiece(Vector2 position, Sprite sprite)
+    {
+        //Set sprite and radius
+        objectRenderer.sprite = sprite;
+        Radius = sprite.bounds.extents.x;
+        CircleCollider2D collider = objectRenderer.GetComponent<CircleCollider2D>();
+        collider.radius = Radius;
+
+        //Set Position
+        objectRenderer.transform.position = position;
+        objectRenderer.gameObject.SetActive(true);
+        isInUse = true;
+    }
+
+    public void RemovePiece()
+    {
+        objectRenderer.gameObject.SetActive(false);
+        isInUse = false;
+    }
+}
